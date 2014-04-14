27bfc1e8b30fda6af54a2e1fa3a16d1475faa9a5
hls47 <hls47@pitt.edu>
2014-04-14 13:38:52 -0400

    Initial Commit

---
+++ Assignment5/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Assignment5
+//
+//  Created by Sinemus, Hannah L on 4/14/14.
+//  Copyright (c) 2014 Sinemus, Hannah L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assignment5Tests/Assignment5Tests.m
@@ -0,0 +1,34 @@
+//
+//  Assignment5Tests.m
+//  Assignment5Tests
+//
+//  Created by Sinemus, Hannah L on 4/14/14.
+//  Copyright (c) 2014 Sinemus, Hannah L. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Assignment5Tests : XCTestCase
+
+@end
+
+@implementation Assignment5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
